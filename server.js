c887255c1532c0ecff865e9d8ccac17a5c5731a4
toshi <yxkxtxsx1075@gmail.com>
2020-10-04 19:55:26 +0900

    Use server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import next from "next";
-import express from "express";
+const next = require("next");
+const express = require("express");
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -23,5 +23,3 @@ app
     })
   )
   .catch(console.error);
-
-export default server;
